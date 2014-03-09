513f6aba0d238b0d03ed81952934246f77c680bb
Preston Lee <conmotto@gmail.com>
2014-03-09 00:21:19 -0700

    Production email fixes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ BitTorious::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'bittorious.com' }
+  config.action_mailer.delivery_method = :sendmail
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
