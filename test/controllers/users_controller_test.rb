f4a70d094557c8f6508597e37f4e03b6592e45d6
Preston Lee <preston.lee@prestonlee.com>
2016-09-08 14:54:57 -0700

    Minor updates and fixes.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -121,8 +121,8 @@ class UsersControllerTest < ActionController::TestCase
   test 'should not render user index as unassigned' do
     log_in :unassigned
     get :index, format: :json
-    assert_response :success
-    get :index
+    # assert_response :success
+    # get :index
     assert_response :unauthorized
   end
 
@@ -130,8 +130,8 @@ class UsersControllerTest < ActionController::TestCase
   test 'should not render user index as subscriber' do
     log_in :subscriber
     get :index, format: :json
-    assert_response :success
-    get :index
+    # assert_response :success
+    # get :index
     assert_response :unauthorized
   end
 
@@ -139,8 +139,8 @@ class UsersControllerTest < ActionController::TestCase
   test 'should not render user index as publisher' do
     log_in :publisher
     get :index, format: :json
-    assert_response :success
-    get :index
+    # assert_response :success
+    # get :index
     assert_response :unauthorized
   end
 
